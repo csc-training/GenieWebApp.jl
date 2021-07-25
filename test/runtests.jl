d71d3aebf8837060d34091f97b3bcc46158f3cc7
Jaan Tollander de Balsch <jaan@hey.com>
2021-07-25 11:02:36 +0300

    Improved unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,19 @@
 using Test
 using Genie
+using HTTP
+
+const app_dir = dirname(@__DIR__)
 
 # TODO: run app in "test" environment
-const root = dirname(@__DIR__)
-Genie.loadapp(root; autostart=false)
+# TODO: set up temporary folder for database
 
-@test 1 == 1
+@testset "Testing GenieWebApp.jl" begin
+    cd(app_dir)
+    loadapp(app_dir; autostart=false)
+    host = "127.0.0.1"
+    port = 8000
+    server = up(port, host; open_browser=false)
+    response = HTTP.request("GET", "http://$(host):$(port)/")
+    @test response.status == 200
+    down()
+end
